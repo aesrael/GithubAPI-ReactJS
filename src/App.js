9649a04ec81efc6b86db16fc5a94fdea1026ad16
Israel Adura <israeladura@gmail.com>
2017-11-10 07:28:20 +0100

    app working!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Repo from './repos';
 //import Search from './search';
-import './App.css';
 //import '/ bootstrap / dist / css / bootstrap.min.css ';
 
 
